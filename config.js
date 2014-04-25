02546226a64e1b73b6d70583ec0da3659cf592a6
tnylea <tnylea@gmail.com>
2014-04-25 09:54:03 -0700

    updating pages with the latest docs

---
+++ config.js
@@ -0,0 +1,34 @@
+// Howdy! This is your documentations configuration file.
+// Just replace your info below, save, and you're done :)
+
+var magnificent = {
+
+  headline : "Ninja Media Script Documentation",
+  name : "Ninja Media Script 1.0.9",
+  short_description : "Your Viral Fun Media Sharing Script",
+  primary_button_url : "http://devdojo.ticksy.com",
+  primary_button_text : "Support Center",
+  // Look for the icon name here: http://fontawesome.io/icons/
+  primary_button_icon : "fa-book",
+  color_scheme : 'light',
+  color : '#F35656',
+
+  view_documentation_button_text : "",
+
+  video_background : false,
+
+  // Each menu item has an associated URL below. Separate each with commas
+  menu_items      :   'Support Center, Our Website, Product Page, Demo Page, Forums',
+  menu_item_urls    :   'http://devdojo.ticksy.com, http://www.devdojo.com, http://codecanyon.net/item/ninja-media-script/6822888, http://ninja-media-script.devdojo.com, http://forums.devdojo.com/conversations/ninja-media-script',
+
+  // Optional social URL's (if left blank, they will not be displayed)
+  facebook				:	'http://www.facebook.com/thedevdojo',
+  twitter				: 	'http://www.twitter.com/thedevdojo',
+  google_plus			: 	'',
+  instagram				: 	'',
+  pinterest 			: 	'http://www.pinterest.com/devdojo',
+  linkedin				: 	'',
+  dribbble				: 	'',
+  github				: 	'http://www.github.com/thedevdojo',
+
+}
